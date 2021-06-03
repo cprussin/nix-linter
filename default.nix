b6798bf36b6b7326759f7e3c6ee8b8c1cafc86b0
Connor Prussin <connor@prussin.net>
2021-06-03 11:59:04 -0700

    NixOS 21.05

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ nixpkgsCommit ? "72b9660dc18ba347f7cd41a9504fc181a6d87dc3", nixpkgsURL ?
+{ nixpkgsCommit ? "eaba7870ffc3400eca4407baa24184b7fe337ec1", nixpkgsURL ?
   "https://github.com/NixOS/nixpkgs/archive/${nixpkgsCommit}.tar.gz"
 , pkgsPath ? builtins.fetchTarball nixpkgsURL, pkgs ? import pkgsPath { } }:
 with pkgs;
